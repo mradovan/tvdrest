be2b7b0e6542e3cc8c63e3b7fed1b641dc152766
Mihael Radovan <mihael.radovan@gmail.com>
2016-06-22 11:08:06 +0200

    Update rest_upload2b.py

--- apexrest/rest_upload2b.py
+++ apexrest/rest_upload2b.py
@@ -1,10 +1,15 @@
 import urllib
+import json
 import urllib2
 import time
 import sys
 from bmp180 import readBmp180
 
-url = 'https://apexea.oracle.com/pls/apex/mradovan/postdata/measvals/'
+urlt = 'https://things.ubidots.com/api/v1.6/devices/ServerRoom/Temperature/values?token=owZKtjFwxoYN83wGrw5gMtwSiyQcOz'
+urlp = 'https://things.ubidots.com/api/v1.6/devices/ServerRoom/Temperature/values?token=owZKtjFwxoYN83wGrw5gMtwSiyQcOz'
+
+ltemp = None
+lpres = None
 
 def getsec():
     t = time.localtime()
@@ -16,23 +21,18 @@ def gettime():
 t.tm_hour, t.tm_min, t.tm_sec)
     return dtime
 
-def getparams(ptime, ptyp, pval):
-    params = urllib.urlencode({
-        'DTIME' : ptime,
-        'MID'   : ptyp,
-        'VAL'   : pval
-    })
-    return params
-
 print('Trivadis Data Uploader for server room 5')
 while 1:
     ltime = gettime()
-    print
     (temperature,pressure) = readBmp180()
-    print('Server room data at %s: Temperature: %s C, Pressure: %s hPa' % (ltime, temperature, pressure))
-    response = urllib2.urlopen(url,getparams(ltime, 'TTEMP', temperature)).read()
-    response = urllib2.urlopen(url,getparams(ltime, 'TPRES', pressure)).read()
+    if ltemp != temperature:
+        ltemp = temperature
+        postdata = {'Temperature': ltemp}
+        req.add_header('Content_Type', 'application/json')
+        data=json.dumps(postdata)
+        response = urllib2.urlopen(urlt,postdata)
+    #if lpres != pressure:
+    #    lpres = pressure
+    #    response = urllib2.urlopen(url,getparams(ltime, 'TPRES', pressure)).read()
     while getsec() > 0:    
-        sys.stdout.write("You have to wait %s seconds to next measure \r" %(60-getsec()))
-        sys.stdout.flush()
         time.sleep(1)
